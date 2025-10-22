1a2df0f3deb74a0c5c56f92a5ccf8ea245c699b5
vladiasa123 <corpodeanvladvlad@gmail.com>
2025-10-22 14:12:01 +0300

    fix

--- wisdom_hr_employee/models/ir_http.py
+++ wisdom_hr_employee/models/ir_http.py
@@ -9,13 +9,22 @@ class IrHttp(models.AbstractModel):
 
         user = self.env.user
         employee = self.env['hr.employee'].sudo().search([('user_id', '=', user.id)], limit=1)
-
         info['department_type'] = employee.department_id.name if employee.department_id else False
 
-        # Fetch published events
-        events = self.env['event.event'].sudo().search([('is_published', '=', True)], limit=5)
-        info['available_events'] = [
-            {"id": e.id, "name": e.name, "date_begin": str(e.date_begin), "date_end": str(e.date_end)}
-            for e in events
-        ]
+        # Fetch published events safely
+        EventModel = self.env.get('event.event')
+        events_list = []
+        if EventModel and 'website_published' in EventModel._fields:
+            events = EventModel.sudo().search([('website_published', '=', True)], limit=5)
+            events_list = [
+                {
+                    "id": e.id,
+                    "name": e.name,
+                    "date_begin": str(e.date_begin),
+                    "date_end": str(e.date_end),
+                }
+                for e in events
+            ]
+        
+        info['available_events'] = events_list
         return info
